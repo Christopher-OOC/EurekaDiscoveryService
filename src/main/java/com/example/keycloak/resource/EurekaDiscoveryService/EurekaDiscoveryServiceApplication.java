b265bd6170beba31accc3bdeb146f8d52f7a52b8
Christopher-OOC <olojedechristopher24@gmail.com>
2024-10-11 19:59:21 +0100

    comit

--- src/main/java/com/example/keycloak/resource/EurekaDiscoveryService/EurekaDiscoveryServiceApplication.java
+++ src/main/java/com/example/keycloak/resource/EurekaDiscoveryService/EurekaDiscoveryServiceApplication.java
@@ -9,7 +9,6 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class EurekaDiscoveryServiceApplication {
 
 	//heyeh
-	//heyeh
 
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaDiscoveryServiceApplication.class, args);
